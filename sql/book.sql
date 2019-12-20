7d5fb34b8eb61573e86d5586bd760f08ee80f8a0
Bryan Tran <tran_b1@denison.edu>
2019-12-20 11:21:44 -0800

    Fixed edit

--- sql/book.sql
+++ sql/book.sql
@@ -4,9 +4,9 @@ CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     authors VARCHAR(255),
     title VARCHAR(255),
-    isbn NUMERIC(20),
+    isbn VARCHAR(255),
     image_url VARCHAR(255),
     description TEXT,
-    bookshelf NUMERIC(20)
+    bookshelf VARCHAR(255)
   );
   
